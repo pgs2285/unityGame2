1fb54122f7ede9c4798844faac53355fc27dd558
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-01-17 22:34:55 +0900

    2023-01-17 / Cut scene 제작 1일차

---
+++ Assets/Scripts/TypingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TypingScript : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] cutSceneScript;
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(_typing());
+    }
+
+    IEnumerator _typing()
+    {
+        
+        for (int j = 0; j < cutSceneScript.Length; j++)
+        {
+            for (int i = 0; i < cutSceneScript[j].Length + 1; i++)
+            {
+                text.text = cutSceneScript[j].Substring(0,i);
+                yield return new WaitForSeconds(0.15f);
+            }
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
