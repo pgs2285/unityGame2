5a67c86534441ace53592af5ede1a6e648919de3
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-01-19 23:40:30 +0900

    2023-01-19 / portal- 2

---
+++ Assets/Scripts/portalManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class portalManager : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("4.Mob");
+        }
+    }
+}
