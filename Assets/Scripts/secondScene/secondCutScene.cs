0efa05ce3595b87d6f075835cccb5463af2ce578
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-02-14 23:20:20 +0900

    2023-02-14 / attackTest, Cutscene Making

---
+++ Assets/Scripts/secondScene/secondCutScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondCutScene : MonoBehaviour
+{
+
+    public GameObject light;
+
+    IEnumerator moveLight()
+    {
+        yield return new WaitForSeconds(3.0f);
+        while (true) {
+            light.transform.position = Vector3.MoveTowards(light.transform.position, new Vector3(0, 10, 0), 0.001f);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        
+        light.transform.position = Vector3.MoveTowards(light.transform.position, new Vector3(0, 10, 0), 0.001f);
+
+    }
+}
