5edfa2e28279ae9253013ed8728b70ac23b012fa
pgs2285 <wldnjs2284@naver.com>
2023-02-15 14:30:38 +0900

    2023-02-15 / 컷신 1차 제작 완료

--- Assets/Scripts/secondScene/secondCutScene.cs
+++ Assets/Scripts/secondScene/secondCutScene.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [System.Serializable]
-public struct moveInfo{
+public struct moveInfo{ 
     public bool isMove;
     
     public GameObject npcID;
@@ -23,6 +24,7 @@ public class secondCutScene : MonoBehaviour
     private void Start(){
         
         if(mvInfo[0].isMove) StartCoroutine(move(0));
+        else StartCoroutine(talk(0));
         
     }
 
@@ -63,6 +65,33 @@ public class secondCutScene : MonoBehaviour
         if(index < mvInfo.Length)
         {
             if(mvInfo[index].isMove) StartCoroutine(move(index));
+            else if(!mvInfo[index].isMove) StartCoroutine(talk(index));
+        } 
+    }
+
+    [SerializeField]
+    TextMeshProUGUI text;
+    [SerializeField]
+    GameObject chatPanel;
+
+    IEnumerator talk(int index){
+        yield return new WaitForSeconds(mvInfo[index].Delay);
+        int textIndex = 0;
+        while(mvInfo[index].context.Length > textIndex){
+
+            chatPanel.SetActive(true);
+            text.text = mvInfo[index].context[textIndex];
+            if(Input.GetKeyDown(KeyCode.Space)) textIndex++;
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        chatPanel.SetActive(false);
+
+        index += 1;
+        if(index < mvInfo.Length)
+        {
+            if(mvInfo[index].isMove) StartCoroutine(move(index));
+            else if(!mvInfo[index].isMove) StartCoroutine(talk(index));
         } 
     }
     
