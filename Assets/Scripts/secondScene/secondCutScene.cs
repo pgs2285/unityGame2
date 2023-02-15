c6413e9c6a1ea03f2d11c619d7a2747940673f20
pgs2285 <wldnjs2284@naver.com>
2023-02-15 14:00:59 +0900

    2023-02-15 / 컷씬 공통스크립트 제작 - 1

--- Assets/Scripts/secondScene/secondCutScene.cs
+++ Assets/Scripts/secondScene/secondCutScene.cs
@@ -2,25 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct moveInfo{
+    public bool isMove;
+    
+    public GameObject npcID;
+    public string direction;
+    public float figure; 
+    public float speed;
+    public float Delay;
+    public string[] context;
+}
+
 public class secondCutScene : MonoBehaviour
 {
+    [SerializeField]
+    public moveInfo[] mvInfo;
 
-    public GameObject light;
 
-    IEnumerator moveLight()
-    {
-        yield return new WaitForSeconds(3.0f);
-        while (true) {
-            light.transform.position = Vector3.MoveTowards(light.transform.position, new Vector3(0, 10, 0), 0.001f);
+    private void Start(){
+        
+        if(mvInfo[0].isMove) StartCoroutine(move(0));
+        
+    }
+
+    Vector3 targetVector;
+    IEnumerator move(int index){
+        yield return new WaitForSeconds(mvInfo[index].Delay);
+        Debug.Log(mvInfo[index].direction);
+        switch(mvInfo[index].direction){
+            case "UP":
+            targetVector = mvInfo[index].npcID.transform.position;
+            targetVector.y += mvInfo[index].figure;
+            break;
+
+            case "DOWN":
+            targetVector = mvInfo[index].npcID.transform.position;
+            targetVector.y -= mvInfo[index].figure;
+            break;
+            
+            case "LEFT":
+            targetVector = mvInfo[index].npcID.transform.position;
+            targetVector.x -= mvInfo[index].figure;
+            break;
+
+            case "RIGHT":
+            targetVector = mvInfo[index].npcID.transform.position;
+            targetVector.x += mvInfo[index].figure;
+            break;
         }
-        
-    }
 
-    // Update is called once per frame
-    void Start()
-    {
+        while(Mathf.Abs(Vector3.Distance(targetVector, mvInfo[index].npcID.transform.position)) > 0.001f){ // 근접시
+            Debug.Log(Vector3.Distance(targetVector, mvInfo[index].npcID.transform.position));
+            mvInfo[index].npcID.transform.position = Vector3.MoveTowards(mvInfo[index].npcID.transform.position, targetVector, mvInfo[index].speed);
+            yield return new WaitForSeconds(0.00001f); // return을 통해 Scene에 진행과정 보이게함
         
-        light.transform.position = Vector3.MoveTowards(light.transform.position, new Vector3(0, 10, 0), 0.001f);
+        }
 
+        index += 1;
+        if(index < mvInfo.Length)
+        {
+            if(mvInfo[index].isMove) StartCoroutine(move(index));
+        } 
     }
+    
 }
