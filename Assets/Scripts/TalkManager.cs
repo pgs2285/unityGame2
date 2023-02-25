f0c2bd851c6c1c86c159ae2070f2917d0d7f6f3d
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-02-26 02:03:10 +0900

    2023-02-26 / 스토리 적용하기 - 2

--- Assets/Scripts/TalkManager.cs
+++ Assets/Scripts/TalkManager.cs
@@ -16,7 +16,8 @@ public class TalkManager : MonoBehaviour
         
         talkData.Add(200, new string[] { "뭘 봐?"});
         /////////////////////////////////캐릭터들의 기본대사 (퀘스트와 관련이 없으면 출력)/////////////////////////////////////
-        talkData.Add(200 + 10, new string[] {"이 동굴에 생명체는 오랜만이구나...", "..."});
+        talkData.Add(200 + 10, new string[] {"??? : \n 이 동굴에 생명체는 오랜만이구나...", "??? : \n 그래, 여긴 어쩌다 오게 되었니?", "이브 : \n 나도 몰라", "이브 : \n 일어나 보니 여기였어."});
+        talkData.Add(200 + 20, new string[] { "??? : \n 배가 고프나 보구나", " ??? : \n ...", "??? : \n 저기 옆에 웅덩이에서 물이라도 마셔보는게 어때?", "??? : \n 웅덩이는 오른쪽으로 쭉가면 있을꺼아." });
         ////////////////////////// 1번퀘스트 종료 ////////////////////////////
         talkData.Add(100 + 20, new string[] { "나무는 빛을 잃었다.", "주변을 다시 둘러보자" });
         //////////////////////////2번퀘스트 종료//////////////////////////////
@@ -24,9 +25,18 @@ public class TalkManager : MonoBehaviour
 
 
     }
-
+    public GameObject effect;
     public string getTalk(int id, int talkIndex){ //GenerateData에서 데이터 가져옴
+        Debug.Log(id);
+        if(talkIndex == talkData[id - id % 10].Length)
+        switch (id) // 특정대사가 끝나고 컷신을 넣어주고 싶으면 여기서
+        {
 
+            case 210:
+                effect.SetActive(true);
+                StartCoroutine(Effect(0));
+                break;
+        }
         if (!talkData.ContainsKey(id))
         {
             if(!talkData.ContainsKey(id - id % 10)){
@@ -53,5 +63,11 @@ public class TalkManager : MonoBehaviour
             return null;
         else
             return talkData[id][talkIndex];
+    }
+
+    IEnumerator Effect(int nun)
+    {
+        yield return new WaitForSeconds(2.0f);
+        effect.SetActive(false);
     }
 }
