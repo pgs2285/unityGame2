dac3a1af34fb86ac0a9984b3c3a6d91727e6b38f
DESKTOP-NOGAGFD\user <wldnjs2284@naver.com>
2023-03-30 23:05:23 +0900

    2023-03-30 / 공격 모션, 콤보 추가

--- Assets/Scripts/SKILL/catRightClickAttack.cs
+++ Assets/Scripts/SKILL/catRightClickAttack.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,24 +13,33 @@ public class catRightClickAttack : MonoBehaviour
     {
         animator = GetComponent<Animator>();
     }
+ 
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) || comboCount > 0)
         {
-            if (Time.time - lastAttackTime < comboResetTime && Input.GetMouseButtonDown(0))
+            Collider2D[] hit = Physics2D.OverlapBoxAll(transform.position, new Vector2(1, 1), 0);
+
+            if (comboCount == 0) lastAttackTime = Time.time; // 처음 들어올때만 찍혀야함
+       
+            if (Time.time - lastAttackTime < comboResetTime)
             {
-                comboCount++;
+                if ( Input.GetMouseButtonDown(0)) comboCount++;
             }
-
-
-            if (comboCount > 3)
+            else
             {
                 comboCount = 0;
             }
 
-            lastAttackTime = Time.time;
-            Debug.Log(comboCount);
+            
+
+            if (comboCount >= 3)
+            {
+                comboCount = 0;
+            }
+            
+   
             // 애니메이션 트리거 설정
             animator.SetInteger("ComboCount", comboCount);
         }
