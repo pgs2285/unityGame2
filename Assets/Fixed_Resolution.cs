23d402fc892821c46fb9b04d3ebdd76d3b8ab4ad
bakjinsoo <bak3482@naver.com>
2023-05-01 16:50:02 +0900

    20230501스타트씬,엔딩씬커밋1차

---
+++ Assets/Fixed_Resolution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fixed_Resolution : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetResolution();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetResolution()
+    {
+        int setWidth = 1366;
+        int setHeight = 768;
+        Screen.SetResolution(setWidth, setHeight,true);
+    }
+}
