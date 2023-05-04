4be1842c404a0f59b8b326382f21774637d7c3c1
pgs2285 <wldnjs2284@naver.com>
2023-05-04 14:49:59 +0900

    2023-05-04 /임시저장

--- Assets/Scripts/UI/Talk.cs
+++ Assets/Scripts/UI/Talk.cs
@@ -12,21 +12,25 @@ public class Talk : MonoBehaviour
     private void Start() {
         player = GameObject.FindWithTag("Player");
         uiManager = GameObject.Find("UI");
+        
     }
 
     public IEnumerator talk(string[] conversation){
         int textIndex = 0;
         uiManager.GetComponent<UIManager>().isAction = true;
         while(conversation.Length > textIndex){
+
             CharacterData.Instance.IsMove = false;
             yield return new WaitForSeconds(0.001f);
-            chatPanel.SetActive(true);
+            // chatPanel.SetActive(true);
+            chatPanel.GetComponent<Animator>().SetBool("isShow",true);
             text.text = conversation[textIndex];
             if(Input.GetKeyDown(KeyCode.Space)) textIndex++;
 
         }
         uiManager.GetComponent<UIManager>().isAction = false;
-        chatPanel.SetActive(false);
+        // chatPanel.SetActive(false);
+        chatPanel.GetComponent<Animator>().SetBool("isShow",false);
         CharacterData.Instance.IsMove = true;
         player.GetComponent<MainCharacter>().OneTime =0;
     }
